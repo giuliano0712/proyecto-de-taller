39fbc946925d785996cbaabdc5a63e10cfc82b3b
giuliano0712 <giulianosicoli@gmail.com>
2024-11-15 11:27:57 -0300

    Update Proyecto_Final_final.ino

--- Proyecto_Final_final.ino
+++ Proyecto_Final_final.ino
@@ -1,9 +1,7 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
-
 // Definir pines del módulo Bluetooth (RX, TX)
 SoftwareSerial bluetooth(10, 11); // RX, TX
-
 // Pines del puente H L298 para los motores traseros
 int motorIzqPinA = 2; //pin a IN2
 int motorIzqPinB = 4; // pin a IN1
@@ -18,21 +16,18 @@ int PotenciaAvance = 150; // potencia de avanze sin giro
 int DiferencialGiro = 100; //potencia de avanze en caso de que haya giro hacia algun lado
 // Configuración del servo
 Servo direccionServo;
-int servoPin = 9;
+int servoPin = 9; // PPin de señal del Servo
 int direccionCentro = 90;  // Posición del servo centrado (90 grados)
 int anguloGiroDer = 135; //Posicion del servo para girar a la derecha
 int anguloGiroIzq = 45; // Posicion del servo para girar izquierda
 // Variables para controlar el estado del motor y la dirección
 int direccionActual = direccionCentro;
 int movimientoActual = 0;  // 0 = detenido, 1 = adelante, -1 = atrás
-
-
 void setup() {
   // Configuración del Bluetooth y del puerto serie
   Serial.begin(9600);
   bluetooth.begin(9600);
   Serial.println("Esperando señal Bluetooth...");
-
   // Configuración de los pines del puente H L298
   pinMode(motorIzqPinA, OUTPUT);
   pinMode(motorIzqPinB, OUTPUT);
@@ -40,34 +35,26 @@ void setup() {
   pinMode(motorDerPinB, OUTPUT);
   pinMode(velocidadIzqPin, OUTPUT);
   pinMode(velocidadDerPin, OUTPUT);
-  
   // Configuración del servo
   direccionServo.attach(servoPin);
   direccionServo.write(direccionCentro);  // Inicializar con el servo centrado
 }
-
 void loop() {
   // Leer la señal Bluetooth
   if (bluetooth.available()) {
     char comando = bluetooth.read(); // Variable que almacena el dato que llega desde la aplicacion
     Serial.print("Comando recibido: ");
     Serial.println(comando);
-    
     // Procesar el comando para controlar la dirección, el movimiento y la potencia de los motores
     procesarComando(comando);
   }
   Potencia();// configurar la potencia suministrada a cada motor trasero
-
   // Aplicar el movimiento actual del motor
   actualizarMovimiento();
-   
-  
   // Aplicar la dirección actual
   direccionServo.write(direccionActual);
-  
   delay(50); // Pequeño retardo para evitar saturación de la comunicación
 }
-
 // Función para procesar el comando recibido
 void procesarComando(char comando) {
   switch (comando) {
@@ -84,7 +71,6 @@ void procesarComando(char comando) {
       potenciader=PotenciaAvance;
        // Detener
       break;
-
     // Movimiento hacia atrás o detener
     case 'B':  // Atrás (botón presionado)
       movimientoActual = -1;
@@ -98,7 +84,6 @@ void procesarComando(char comando) {
       potenciader=PotenciaAvance;
         // Detener
       break;
-
     // Control de dirección (giro a la izquierda, derecha o centrar)
     case 'L':  // Girar a la izquierda (botón presionado)
       direccionActual = anguloGiroIzq;
@@ -126,10 +111,8 @@ void procesarComando(char comando) {
       break;
   }
 }
-
 // Función para ajustar las velocidades de los motores al girar
 void Potencia(){
-
   if(movimientoActual==1){  // en caso de que el motor avance las potencias de giro son acordes al giro
   analogWrite(velocidadIzqPin,potenciaizq); 
   analogWrite(velocidadDerPin,potenciader);
@@ -143,7 +126,6 @@ void Potencia(){
   analogWrite(velocidadDerPin,potenciader);
   }
 }
-
 // Función para actualizar el movimiento de los motores traseros según el estado
 void actualizarMovimiento() {
   switch (movimientoActual) {
